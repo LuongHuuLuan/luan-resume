52e8e8489167cb039e56aa963b5055c8749eb315
LuongHuuLuan <luongluanmpt@gmail.com>
2024-05-31 17:45:36 +0700

    download cv

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { transition1 } from "@/app/transition";
 
 type Props = {}
 
 export default function Contact({}: Props) {
   return (
-    <section className='section'>Contact</section>
+<motion.section
+      className="section"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition1}
+    >
+      {/* text and image wrapper */}
+      <div className="container mx-auto h-full relative">
+        <div className="flex flex-col justify-center items-center h-full">
+          {/* text */}
+          <motion.div
+            className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start"
+            initial={{ opacity: 0, y: "-50%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-50%" }}
+            transition={transition1}
+          >
+            <h1 className="h1">Contact</h1>
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
